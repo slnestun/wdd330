808814426a0938025b6843c6b89b11982601864e
slnestun <riv21007@byui.edu>
2026-09-12 23:07:39 -0300

    Add a superscript number of items in the cart to the backpack icon

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, updateCartCount } from "./utils.mjs";
 
 export default class ProductDetails {
   constructor(productId, dataSource) {
@@ -30,6 +30,7 @@ export default class ProductDetails {
     }
 
     setLocalStorage("so-cart", cart);
+    updateCartCount();
   }
 
   renderProductDetails() {
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,12 @@
-import { getLocalStorage } from "./utils.mjs";
+import { updateCartCount } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-}
+  const productList = document.querySelector(".product-list");
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: ${item.quantity}</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
+  if (productList) {
+    productList.innerHTML = "";
+  }
 }
 
 renderCartContents();
+updateCartCount();
